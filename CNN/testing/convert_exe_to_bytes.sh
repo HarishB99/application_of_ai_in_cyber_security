8a76309580d723f0d974c1506a743d2a22ac7265
Harish <harish_balamurugan@yahoo.com.sg>
2019-03-26 23:18:12 -0700

    Rectify logic error in convert_exe_to_bytes.sh

--- CNN/testing/convert_exe_to_bytes.sh
+++ CNN/testing/convert_exe_to_bytes.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
+mkdir "$(pwd)/test_cases"
+
 for i in $(ls -a ./test_cases_exe/*.exe); do
     filename=$(basename $i);
     filename="${filename%.*}"
-    mkdir "$(pwd)/test_cases"
     xxd $i | cut -d' ' -f-9 > "$(pwd)/test_cases/$filename.bytes";
 done
