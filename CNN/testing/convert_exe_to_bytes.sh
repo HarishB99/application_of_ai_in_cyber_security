7f3796fbe6421cea769d3a98f909d5bbaed44d2d
Harish Balamurugan <harishbalamurugan@pop-os.localdomain>
2019-04-01 15:12:53 +0800

    Remove unnecessary script

--- CNN/testing/convert_exe_to_bytes.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-mkdir "$(pwd)/test_cases"
-
-for i in $(ls -a ./test_cases_exe/*.exe); do
-    filename=$(basename $i);
-    filename="${filename%.*}"
-    xxd $i | cut -d' ' -f-9 > "$(pwd)/test_cases/$filename.bytes";
-done
